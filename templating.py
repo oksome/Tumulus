d4e526abe94d748f0cdba3a472aabf3b0bceef36
OKso <user+github@okso.me>
2013-11-12 23:34:25 +0100

    Started templating

---
+++ templating.py
@@ -0,0 +1,19 @@
+import html
+import time
+
+import tumulus.tags as t
+import tumulus.formulas as f
+import tumulus.embed as e
+
+
+def template(filename, **locals):
+    path = 'templates/{}'.format(filename)
+    with open(path) as source:
+        code = compile(source.read(), path, 'eval')
+        return eval(code, {'t': t, 'f': f, 'e': e,
+                           'escape': html.escape, 'ctime': time.ctime},
+                    locals)
+
+
+def build_template(filename, **locals):
+    return template(filename, **locals).build()
