96821965f473f753512bda21c28803782d0cc9ff
OKso <user+github@okso.me>
2013-11-19 01:25:40 +0100

    Added Injection tests

---
+++ tests/test_injections.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of Tumulus.
+#
+# Copyright (C) 2013 OKso (http://okso.me)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import tumulus.tags as t
+
+
+def test_no_injection():
+    r = t.html()
+    assert r
+    assert r.injections() == []
+
+
+def test_injection():
+    r = t.html(injections=['trucmuche'])
+    assert r
+    assert r.injections() == ['trucmuche']
