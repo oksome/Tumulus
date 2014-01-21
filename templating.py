d21ff0f22ddfd15113bac6197883dba3545437f1
OKso <user+github@okso.me>
2014-01-21 13:22:16 +0100

    Switched default templates from eval to exec

--- templating.py
+++ templating.py
@@ -7,11 +7,28 @@ import tumulus.embed as e
 
 
 def template(filename, **locals):
+    ''' Returns '''
+    path = 'templates/{}'.format(filename)
+    with open(path) as source:
+        code = compile(source.read(), path, 'exec')
+        global_vars = {'t': t,
+                       'f': f,
+                       'e': e,
+                       'escape': html.escape,
+                       'ctime': time.ctime}
+        exec(code, global_vars, locals)
+        return locals['page']
+
+
+def template_eval(filename, **locals):
     path = 'templates/{}'.format(filename)
     with open(path) as source:
         code = compile(source.read(), path, 'eval')
-        return eval(code, {'t': t, 'f': f, 'e': e,
-                           'escape': html.escape, 'ctime': time.ctime},
+        return eval(code, {'t': t,
+                           'f': f,
+                           'e': e,
+                           'escape': html.escape,
+                           'ctime': time.ctime},
                     locals)
 
 
