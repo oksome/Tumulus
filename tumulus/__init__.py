2fc9e819688d843eeaf7dcd60ebfe11ec086e574
OKso <user+github@okso.me>
2014-02-08 22:58:58 +0100

    Increased version number

--- tumulus/__init__.py
+++ tumulus/__init__.py
@@ -16,3 +16,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "OKso <okso.me>"
+__version__ = '0.1.2'
