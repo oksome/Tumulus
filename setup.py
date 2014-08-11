1e777b4c2e68c96cee18db0a170207338f11754d
OKso <user+github@okso.me>
2014-08-11 02:03:07 +0200

    Increased version number for bugfix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='Tumulus',
-      version='0.2.0',
+      version='0.2.1',
       description='Python HTML Generator for Recyclable Web Elements',
       long_description=long_description,
       author='OKso.me',
